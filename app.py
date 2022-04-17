44b4ad1261a213fb65dc2e0572951506770096e5
Departures <muhammadhafid1424@gmail.com>
2022-04-17 18:56:51 +0700

    latest

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ TOKEN = os.environ.get("TELEGRAM_ID")
 def start(update, context):
     yourname = update.message.chat.first_name
 
-    msg = "Hi "+yourname+"! Welcome to mimic bot."
+    msg = "Hi "+yourname+"! Welcome to pascabot bot."
     context.bot.send_message(update.message.chat.id, msg)
 
 #Message handler for texts only
@@ -39,7 +39,7 @@ def main():
     dp.add_handler(CommandHandler("start",start))
     dp.add_handler(CommandHandler("details", details))
 
-    dp.add_handler(MessageHandler(Filters.text, mimic))
+    dp.add_handler(MessageHandler(Filters.text, pascabot))
 
 
     dp.add_error_handler(error)
@@ -47,7 +47,7 @@ def main():
     #to start webhook
     updater.start_webhook(listen="0.0.0.0",port=os.environ.get("PORT",443),
                           url_path=TOKEN,
-                          webhook_url="https://mimic-app.herokuapp.com/"+TOKEN)
+                          webhook_url="https://pascabot.herokuapp.com/"+TOKEN)
     updater.idle()
 
 #start application with main function
